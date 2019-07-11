bb132638d42423ea117415fb923309f007ece128
maehoad <maehoad@gmail.com>
2019-07-11 16:42:16 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Components/Header";
+import About from "./Components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = require("./resumeData.json");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
+  }
+
+  getResumeData() {
+    this.setState({ resumeData: data });
+  }
+
+  componentDidMount() {
+    {
+      /* After the component hows up on the screen*/
+    }
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main} />
+        {/* <About data={this.state.resumeData.main} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
